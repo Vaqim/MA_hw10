df77a69aaf566d55ad996995d563f9fda922a99a
Vaqim <vadimser1355@gmail.com>
2021-01-18 14:31:48 +0200

    migration file update

--- src/db/migrations/20210111131613_createOrderSystemTables.js
+++ src/db/migrations/20210111131613_createOrderSystemTables.js
@@ -1,37 +1,29 @@
 exports.up = async (knex) => {
-  // colors
-  // types
-  // products
-  // orders
-  // orderItems
+  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
 
-  const createExt = knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-
-  const createFunc = knex.schema.raw(`
-  CREATE OR REPLACE FUNCTION updated_at_timastamp() RETURNS TRIGGER
-    LANGUAGE plpgsql
-    AS
-    $$
-    BEGIN
-      NEW.updated_at = CURRENT_TIMESTAMP;
-      RETURN NEW;
-    END;
-    $$;
-  `);
-
-  const createColors = knex.schema.createTable('colors', (table) => {
-    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-    table.string('name').notNullable().unique();
-    table.timestamp('deleted_at').nullable();
-  });
-
-  const createTypes = knex.schema.createTable('types', (table) => {
-    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-    table.string('name').notNullable().unique();
-    table.timestamp('deleted_at').nullable();
-  });
-
-  await Promise.all([createExt, createFunc, createColors, createTypes]);
+  await Promise.all([
+    knex.schema.raw(`
+      CREATE OR REPLACE FUNCTION updated_at_timastamp() RETURNS TRIGGER
+        LANGUAGE plpgsql
+        AS
+        $$
+        BEGIN
+          NEW.updated_at = CURRENT_TIMESTAMP;
+          RETURN NEW;
+        END;
+        $$;
+    `),
+    knex.schema.createTable('colors', (table) => {
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
+      table.string('name').notNullable().unique();
+      table.timestamp('deleted_at').nullable();
+    }),
+    knex.schema.createTable('types', (table) => {
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
+      table.string('name').notNullable().unique();
+      table.timestamp('deleted_at').nullable();
+    }),
+  ]);
 
   await knex.schema.createTable('products', (table) => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
@@ -43,62 +35,57 @@ exports.up = async (knex) => {
     table.timestamps(true, true);
   });
 
-  const addConstraint = knex.schema.raw(
-    'ALTER TABLE products ADD CONSTRAINT uniq_product UNIQUE(color_id, type_id, price)',
-  );
-
-  const createTrigger = knex.schema.raw(`
-  CREATE TRIGGER updated_at_timastamp
-  BEFORE UPDATE
-  ON products
-  FOR EACH ROW
-  EXECUTE PROCEDURE updated_at_timastamp();
-  `);
-
-  const createStatusType = knex.schema.raw(
-    "CREATE TYPE status AS ENUM ('pending', 'confirmed', 'cancelled')",
-  );
-
-  await Promise.all([addConstraint, createTrigger, createStatusType]);
-
-  await knex.schema.createTable('users', (table) => {
-    table.integer('id').notNullable().primary();
-    table.string('name').notNullable().unique();
-  });
+  await Promise.all([
+    knex.schema.raw(`
+      CREATE TRIGGER updated_at_timastamp
+      BEFORE UPDATE
+      ON products
+      FOR EACH ROW
+      EXECUTE PROCEDURE updated_at_timastamp();
+    `),
+    knex.schema.createTable('users', (table) => {
+      table.integer('id').notNullable().primary();
+      table.string('name').notNullable().unique();
+    }),
+  ]);
 
   await knex.schema.createTable('orders', (table) => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
     table.integer('user_id').notNullable().unique().references('users.id');
-    table.string('from').nullable();
-    table.string('to').nullable();
+    table
+      .enu('status', ['pending', 'confirmed', 'cancelled'], {
+        enumName: 'order_status',
+        useNative: true,
+      })
+      .notNullable()
+      .defaultTo('pending');
   });
-
-  await knex.schema.raw(`ALTER TABLE orders ADD COLUMN current_status STATUS DEFAULT 'pending'`);
 
   await knex.schema.createTable('order_item', (table) => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
     table.uuid('order_id').references('orders.id').notNullable();
     table.uuid('product_id').references('products.id').notNullable();
     table.integer('quantity').defaultTo(0).notNullable();
+    table.unique(['order_id', 'product_id'], 'uniq_order_item');
   });
-  await knex.schema.raw(
-    'ALTER TABLE order_item ADD CONSTRAINT uniq_order_item UNIQUE(order_id, product_id)',
-  );
 };
 
 exports.down = async (knex) => {
   await knex.schema.dropTable('order_item');
   await knex.schema.dropTable('orders');
 
-  await knex.schema.dropTable('users');
+  await Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.raw('DROP TYPE IF EXISTS status'),
+    knex.schema.dropTable('products'),
+  ]);
 
-  await knex.schema.raw('DROP TYPE IF EXISTS status');
-
-  await knex.schema.dropTable('products');
-  await knex.raw('DROP FUNCTION IF EXISTS updated_at_timastamp()');
-
-  await knex.schema.dropTable('colors');
-  await knex.schema.dropTable('types');
+  await Promise.all([
+    knex.raw('DROP FUNCTION IF EXISTS updated_at_timastamp()'),
+    knex.schema.dropTable('colors'),
+    knex.schema.dropTable('types'),
+    knex.raw('DROP TYPE IF EXISTS order_status'),
+  ]);
 
   await knex.raw('DROP EXTENSION IF EXISTS "uuid-ossp"');
 };
